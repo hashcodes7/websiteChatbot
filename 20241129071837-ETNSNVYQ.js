0af5110ab4198072fd992978b465401432a439d9
Harsh Vardhan Verma <83136005+hashcodes7@users.noreply.github.com>
2024-12-09 15:18:48 +0530

    Create 20241129071837-ETNSNVYQ.js

---
+++ 20241129071837-ETNSNVYQ.js
@@ -0,0 +1,20 @@
+window.botpress.init({
+  "botId": "80b42f23-6ad4-467a-8c56-028b48e5ce78",
+  "configuration": {
+    "composerPlaceholder": "How Can I help you today?",
+    "botName": "Flex-Test-BOT",
+    "botAvatar": "https://files.bpcontent.cloud/2024/11/29/07/20241129072502-HQ2OHDZO.webp",
+    "website": {},
+    "email": {},
+    "phone": {},
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#3B82F6",
+    "variant": "solid",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 1,
+    "additionalStylesheetUrl": "https://files.bpcontent.cloud/2024/11/29/09/20241129094823-S33LQXFO.css"
+  },
+  "clientId": "ceb32ba0-e7bf-4fa6-a839-0359276c839f"
+});
